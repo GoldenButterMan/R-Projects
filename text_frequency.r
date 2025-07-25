9f264b4d61115115a7815097f470ab35a97e1ad3
GoldenButterMan <spencer-peterson@live.com>
2025-07-25 11:18:59 -0700

    initial work on a program that counts the frequency of words in a text file

---
+++ text_frequency.r
@@ -0,0 +1,30 @@
+#install needed libraries if missing
+if(!require(tidytext) | !require(magrittr) | !require(dplyr) | !require(ggplot2)){
+    install.packages("tidytext");
+    install.packages("magrittr");
+    install.packages("dplyr");
+    install.packages("ggplot2");
+}
+library("tidytext");
+library("magrittr");
+library("dplyr");
+library("ggplot2");
+
+#user input for file name
+userPrompt <- readline(prompt = "Enter file name: ");
+file <- readLines(userPrompt);
+
+#make dataframe and convert to a list of words
+text <- data.frame(line = 1:length(file), text = file);
+wordList <- text %>%
+    unnest_tokens(word, text);
+
+data("stop_words");
+
+wordList <- wordList %>%
+    anti_join(stop_words, by = "word");
+
+wordFrequency <- wordList %>%
+    count(word, sort = TRUE);
+
+print(wordFrequency);
