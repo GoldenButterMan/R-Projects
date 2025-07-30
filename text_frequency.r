c98eab1a963dda98cacacd864bee88639f123801
GoldenButterMan <spencer-peterson@live.com>
2025-07-30 12:50:43 -0700

    reformatted to make cleaner to read

--- text_frequency.r
+++ text_frequency.r
@@ -1,5 +1,5 @@
-#install needed libraries if missing
-if(!require(tidytext) || !require(magrittr) || !require(dplyr) || !require(ggplot2)){
+# install needed libraries if missing
+if (!require(tidytext) || !require(magrittr) || !require(dplyr) || !require(ggplot2)) {
     install.packages("tidytext")
     install.packages("magrittr")
     install.packages("dplyr")
@@ -10,25 +10,25 @@ library("magrittr")
 library("dplyr")
 library("ggplot2")
 
-#user input for file name
+# user input for file name
 user_prompt <- readline(prompt = "Enter file name: ")
 file <- readLines(user_prompt)
 
-#make dataframe and convert to a list of words
+# make dataframe and convert to a list of words
 text <- data.frame(line = seq_along(file), text = file)
 word_list <- text %>%
-  unnest_tokens(word, text)
+    unnest_tokens(word, text)
 
 word_frequency <- word_list %>%
-  count(word, sort = TRUE)
+    count(word, sort = TRUE)
 
 print(word_frequency)
 
-#graph word frequency as bar graph
+# graph word frequency as bar graph
 
 print(
-ggplot(word_frequency, aes(x = reorder(word, n), y = n)) +
-  geom_col(fill = "green") +
-  coord_flip() +
-  labs(title = "Top 10 frequent Words", x = "Word", y = "Frequency")
+    ggplot(word_frequency, aes(x = reorder(word, n), y = n)) +
+        geom_col(fill = "green") +
+        coord_flip() +
+        labs(title = "Top 10 frequent Words", x = "Word", y = "Frequency")
 )
